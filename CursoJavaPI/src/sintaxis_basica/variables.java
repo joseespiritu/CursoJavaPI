e307deb61e3bf4cd96396c09cda0dfe90f818b8d
joseespiritu <joseespiritu@aragon.unam.mx>
2020-04-24 19:10:40 -0500

    declaracion de variables

---
+++ CursoJavaPI/src/sintaxis_basica/variables.java
@@ -0,0 +1,9 @@
+package sintaxis_basica;
+
+public class variables {
+    public static void main(String[] args) {
+        int edad = 35;
+        edad = 75;
+        System.out.println(edad);
+    }
+}
