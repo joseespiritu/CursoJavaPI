fe17b6273d8210b2731a48ffb7090d91b7a7819c
joseespiritu <joseespiritu89@aragon.unam.mx>
2020-09-03 20:14:43 -0500

    P.O.O. creacion de clase e instancia de clase (ejemplar de clase)

---
+++ POO/src/poo/Uso_Coche.java
@@ -0,0 +1,14 @@
+package poo;
+
+public class Uso_Coche {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Coche Renault=new Coche(); //Instanciar una clase, Ejemplar de clase
+		
+		System.out.println("Este coche tiene: " + Renault.ruedas + " ruedas");
+
+	}
+
+}
