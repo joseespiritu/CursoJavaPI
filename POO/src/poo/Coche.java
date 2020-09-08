a4938b753f93c50c854cbac973d9606f07c60f27
joseespiritu <joseespiritu89@aragon.unam.mx>
2020-09-07 20:58:13 -0500

    Getters y Setters, sintaxis y ejemplos

--- POO/src/poo/Coche.java
+++ POO/src/poo/Coche.java
@@ -2,11 +2,17 @@ package poo;
 
 public class Coche {
 	
+	//Caracteristicas comunes
 	private int ruedas;
 	private int largo;
 	private int ancho;
 	private int motor;
-	private int peso;
+	private int peso_plataforma;
+	
+	//Propiedades que pueden variar
+	String color;
+	int peso_total;
+	boolean asientos_cuero, climatizador;
 	
 	public Coche() {
 		
@@ -14,8 +20,20 @@ public class Coche {
 		largo=2000;
 		ancho=300;
 		motor=1600;
-		peso=500;
+		peso_plataforma=500;
 		
+	}
+	
+	public String dime_largo() { // GETTER
+		return "El largo del coche es " + largo;
+	}
+	
+	public void establece_color() { // SETTER
+		color="Azul";
+	}
+	
+	public String dime_color() {
+		return "El color del coche es " + color;
 	}
 	
 }
--- POO/src/poo/Uso_Coche.java
+++ POO/src/poo/Uso_Coche.java
@@ -5,9 +5,15 @@ public class Uso_Coche {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Coche Renault=new Coche(); //Instanciar una clase, Ejemplar de clase
+		//Coche Renault=new Coche(); //Instanciar una clase, Ejemplar de clase		
+		//System.out.print(Renault.dime_largo());
+		// System.out.println("Este coche tiene: " + Renault.largo + " ruedas");
 		
-		System.out.println("Este coche tiene: " + Renault.ruedas + " ruedas");
+		Coche micoche = new Coche();
+		
+		//micoche.establece_color();
+		
+		System.out.println(micoche.dime_color());
 
 	}
 
